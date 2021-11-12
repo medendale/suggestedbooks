47c29bcf5ef1302ec59e6764069d61e9cbc44e22
medendale <70830116+medendale@users.noreply.github.com>
2021-11-11 22:21:55 -0500

    aded search

--- app.py
+++ app.py
@@ -58,6 +58,26 @@ def clean_price(price_str):
               ''')
     else:
         return int(price_float * 100)
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+              \n***** ID ERROR *****
+              \rThe Id format should include a valid Id.
+              \rPress enter to try again.
+              ''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input('''
+              \n***** ID ERROR *****
+              \roptions: {options}.
+              \rPress enter to try again.
+              ''')
+            return 
 
 def add_csv():
     with open('suggested_books.csv') as csvfile:
@@ -108,7 +128,24 @@ def app():
             input('\nPress enter to return to main menu.')
         elif choice == '3':
             #Search_book()
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input('''
+                    \nId Options:{id_options}
+                    \rBook Id: ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                        id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+                  \n{the_book.title} {the_book.author} 
+                  \rPublished: {the_book.published_date} 
+                  \rPrice: ${the_book.price /100 }''')
+            input('\nPress enter to return to main menu.')
+            
         elif choice == '4':
             #Book_Analysis()
             pass
